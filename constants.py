994d3ac9dff31829e6441a5a3fab67c45cee6636
Txuluito <54414625+Txuluito@users.noreply.github.com>
2026-03-01 12:57:38 +0100

    configuracion personal

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@
 URL_WEB_APP = "https://script.google.com/macros/s/AKfycbx06kDF-GHqgiLX8bUzsDKKhG3eD0VvxaeV89Lhv8mjEUiXlKo7bEOgPN7RURwk60NBtg/exec"
 
 # ID de la hoja de cálculo de Google Sheets
-SHEET_ID = "1OUstNEtKxroFlveZDWUSSrwA2iydx3Ubbhs5ElIsVj2nZZhxRGYzdl5D"
+SHEET_ID = "1djxP-ym83LwSVwVWkgtiMcafzQfHnHYeoJbzrD0jCow"
 
 # Mostrar u ocultar secciones
 SHOW_BIO_ANALYSIS = False
